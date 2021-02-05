efe7bb52b89963a027f58d852a7f467741b6ca77
michaliwaniec <mich6h94@stud.kea.dk>
2021-02-05 16:41:47 +0100

    base for game

--- script.js
+++ script.js
@@ -8,8 +8,79 @@ const scissors = document.querySelector(".scissors");
 const win = document.querySelector("#win");
 const lose = document.querySelector("#lose");
 const draw = document.querySelector("#draw");
-const player1 = document.querySelector("#plater1.player");
-const player2 = document.querySelector("#plater2.player");
+const player1 = document.querySelector("#player1.player");
+const player2 = document.querySelector("#player2.player");
 
 function playGame() {
+  rock.addEventListener("click", () => {
+    player1.classList.add("shake");
+    player2.classList.add("shake");
+    rockPick();
+  });
+  paper.addEventListener("click", () => {
+    player1.classList.add("shake");
+    player2.classList.add("shake");
+    paperPick();
+  });
+  scissors.addEventListener("click", () => {
+    player1.classList.add("shake");
+    player2.classList.add("shake");
+    scissorsPick();
+  });
+  setTimeout(computerDecide, 1800);
+}
 
+function rockPick() {
+  console.log("rock is here");
+  rock.removeEventListener("click", () => {
+    player1.classList.add("shake");
+    player2.classList.add("shake");
+    rockPick();
+  });
+  document
+    .querySelector(".player.shake")
+    .addEventListener("animationend", computerDecide);
+  document.querySelector.addEventListener("animationend", computerDecide());
+}
+
+function paperPick() {
+  console.log("paper is here");
+  rock.removeEventListener("click", () => {
+    player1.classList.add("shake");
+    player2.classList.add("shake");
+    paperPick();
+  });
+  document
+    .querySelector(".player.shake")
+    .addEventListener("animationend", computerDecide);
+  document.querySelector.addEventListener("animationend", computerDecide());
+}
+
+function scissorsPick() {
+  console.log("scissors is here");
+  rock.removeEventListener("click", () => {
+    player1.classList.add("shake");
+    player2.classList.add("shake");
+    scissorsPick();
+  });
+  document
+    .querySelector(".player.shake")
+    .addEventListener("animationend", computerDecide);
+  document.querySelector.addEventListener("animationend", computerDecide());
+}
+
+let computer = "";
+function computerDecide() {
+  console.log("computer is deciding");
+  let number = Math.floor(Math.random() * 3);
+  if (number == 0) {
+    computer = "rock";
+    console.log("rockai");
+  } else if (number == 1) {
+    computer = "paper";
+    console.log("paperai");
+  } else if (number == 2) {
+    computer = "scissors";
+    console.log("scissorsai");
+  }
+}
